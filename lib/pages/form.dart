d76b1262ef804eb89572792ab175a13ec1c5bf0e
kinlay <kinleyraperlhendup@gmail.com>
2024-07-25 15:41:40 +0600

    form added

---
+++ lib/pages/form.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+
+class Registration extends StatefulWidget {
+  const Registration({super.key});
+
+  @override
+  State<Registration> createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _firstName = TextEditingController();
+  TextEditingController _lastName = TextEditingController();
+  TextEditingController _email = TextEditingController();
+  TextEditingController _password = TextEditingController();
+  TextEditingController _rePassword = TextEditingController();
+  bool isObs = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Registration",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                /// First Name
+                TextFormField(
+                  controller: _firstName,
+                  decoration: InputDecoration(
+                    label: Text("First Name"), hintText: "First Name",
+                    // hintStyle: TextStyle(color: Colors.purple)
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    // suffix: Icon(Icons.search),                    // suffixIcon: Icon(Icons.abc),
+                    // prefix: Icon(Icons.abc),                    // prefixIcon: Icon(Icons.abc)
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter first name";
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+
+                /// Last Name
+                TextFormField(
+                  controller: _lastName,
+                  decoration: InputDecoration(
+                    label: Text("Last Name"), hintText: "Last Name",
+                    // hintStyle: TextStyle(color: Colors.purple)
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    // suffix: Icon(Icons.search),                    // suffixIcon: Icon(Icons.abc),
+                    // prefix: Icon(Icons.abc),                    // prefixIcon: Icon(Icons.abc)
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      // return "Please enter last name";
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+
+                /// Email
+                TextFormField(
+                  controller: _email,
+                  decoration: InputDecoration(
+                    label: Text("Email"), hintText: "Enter your email",
+                    // hintStyle: TextStyle(color: Colors.purple)
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    suffix: Icon(
+                        Icons.email_outlined), // suffixIcon: Icon(Icons.abc),
+                    // prefix: Icon(Icons.abc),                    // prefixIcon: Icon(Icons.abc)
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter email";
+                    }
+                    if (!value.contains("@")) {
+                      return "Enter correct email";
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+
+                /// Password
+                TextFormField(
+                  controller: _password,
+                  obscureText: isObs,
+                  decoration: InputDecoration(
+                    label: Text("Password"),
+                    hintText: "Enter your password",
+                    // hintStyle: TextStyle(color: Colors.purple)
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    suffix: IconButton(
+                      onPressed: () {
+                        isObs = !isObs;
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.visibility),
+                    ),
+                    // suffixIcon: Icon(Icons.abc),                    // prefix: Icon(Icons.abc),
+                    // prefixIcon: Icon(Icons.abc)
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter password";
+                    }
+                    if (value.length < 8) {
+                      return "Password must be at least 8 digits/char";
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+
+                /// Re-password
+                TextFormField(
+                  controller: _rePassword,
+                  obscureText: isObs,
+                  decoration: InputDecoration(
+                    label: Text("Re enter the Password"),
+                    hintText: "Re enter your password",
+                    // hintStyle: TextStyle(color: Colors.purple)
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    suffix: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isObs = !isObs;
+                        });
+                      },
+                      icon: Icon(Icons.visibility),
+                    ), // suffixIcon: Icon(Icons.abc),
+                    // prefix: Icon(Icons.abc),                    // prefixIcon: Icon(Icons.abc)
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter password";
+                    }
+                    if (value != _password.text.toString()) {
+                      return "Password does not match";
+                    }
+                  },
+                ),
+                MaterialButton(
+                  minWidth: MediaQuery.of(context).size.width * 0.8,
+                  color: Colors.blue,
+                  onPressed: () {
+                    _formKey.currentState!.validate();
+                  },
+                  child: Text("Register Now!"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
